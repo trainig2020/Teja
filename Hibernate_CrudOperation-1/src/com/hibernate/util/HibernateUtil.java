6a3ba5b40a376ed0da7f233b346ee4ff7e08c596
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-07-18 17:57:12 +0530

    Add files via upload

---
+++ Hibernate_CrudOperation-1/src/com/hibernate/util/HibernateUtil.java
@@ -0,0 +1,33 @@
+package com.hibernate.util;
+
+import java.util.Properties;
+
+import javax.imageio.spi.ServiceRegistry;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import com.hibernate.model.Department;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return sessionFactory.openSession();
+    }
+
+    public static void shutdown() {
+        sessionFactory.close();
+    }
+}
